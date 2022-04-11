c99c4f00b4bdfa5fff3093ab0bbc420a6d07207b
QDistinction <88264325+mohsinjamal@users.noreply.github.com>
2022-04-11 06:05:26 +0000

    Update disablev6.sh

--- disablev6.sh
+++ disablev6.sh
@@ -1,11 +1,9 @@
-sed -i '/net.ipv6.conf.all.disable_ipv6/d' /etc/sysctl.d/99-sysctl.conf
-sed -i '/net.ipv6.conf.default.disable_ipv6/d' /etc/sysctl.d/99-sysctl.conf
-sed -i '/net.ipv6.conf.lo.disable_ipv6/d' /etc/sysctl.d/99-sysctl.conf
-sed -i '/net.ipv6.conf.all.disable_ipv6/d' /etc/sysctl.conf
-sed -i '/net.ipv6.conf.default.disable_ipv6/d' /etc/sysctl.conf
-sed -i '/net.ipv6.conf.lo.disable_ipv6/d' /etc/sysctl.conf
+sudo sed -i '/net.ipv6.conf.all.disable_ipv6/d' /etc/sysctl.conf
+sudo sed -i '/net.ipv6.conf.default.disable_ipv6/d' /etc/sysctl.conf
+sudo sed -i '/net.ipv6.conf.lo.disable_ipv6/d' /etc/sysctl.conf
 
-echo "net.ipv6.conf.all.disable_ipv6 = 1
+sudo echo "net.ipv6.conf.all.disable_ipv6 = 1
 net.ipv6.conf.default.disable_ipv6 = 1
-net.ipv6.conf.lo.disable_ipv6 = 1" >>/etc/sysctl.d/99-sysctl.conf
-sysctl --system
+net.ipv6.conf.lo.disable_ipv6 = 1" >>/etc/sysctl.conf
+
+sudo sysctl -p /etc/sysctl.conf
