175a6ccefdc73c7883a86f1918e50020b6593638
QDistinction <88264325+mohsinjamal@users.noreply.github.com>
2022-04-11 06:27:16 +0000

    Update disablev6.sh

--- disablev6.sh
+++ disablev6.sh
@@ -7,3 +7,9 @@ net.ipv6.conf.default.disable_ipv6 = 1
 net.ipv6.conf.lo.disable_ipv6 = 1" >>/etc/sysctl.conf
 
 sudo sysctl -p /etc/sysctl.conf
+
+sudo cat >> /etc/default/grub <<EOF
+GRUB_CMDLINE_LINUX="ipv6.disable=1"
+EOF
+
+sudo update-grub || sudo grub2-mkconfig -o /boot/grub2/grub.cfg
